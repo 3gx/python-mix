78b5cf8e37f3d94663a4c658d4f3608d144ccffb
Evghenii <egx@egx-mbp.local>
2020-10-06 16:22:05 -0700

    add stuff

--- pmatch/ex1.py
+++ pmatch/ex1.py
@@ -1,45 +1,81 @@
 #type: ignore
 
 from __future__ import annotations
-from dataclasses import dataclass
-from typing import Union, Callable
+import dataclasses as _dc
+#from dataclasses import dataclass
+from typing import Union, Callable as Lam
 
-@dataclass
+@_dc.dataclass
 class S:
     _1 : int
 
-@dataclass
-class T:
-    _1 : float
-    _2 : str
-    _3 : int
+
+def dataclass(*types):
+    def wrapper(cls):
+        fields = [(f"_{i}",ty) for i, ty in enumerate(types)]
+        string = f"{cls.__name__}("
+        keys = [f"_{i}" for i,_ in enumerate(types)]
+        for k in keys[:-1]:
+            string += f"{{self.{k}}},"
+        string += f"{{self.{keys[-1]}}})"
+        def get_names(self):
+            string = f"{cls.__name__}("
+            for i, k in enumerate(keys):
+                value = getattr(self,k)
+                if isinstance(value, str):
+                    string += f"'{value}'"
+                else:
+                    string += f"{value}"
+                string += ")" if (i == len(keys)-1) else ","
+            return string
+        return _dc.make_dataclass(cls.__name__, fields=fields,
+                namespace={'__repr__': lambda self: get_names(self)})
+    return wrapper
 
 
-@dataclass
-class U:
-    _1 : float
+@dataclass(float, str,int)
+class T: pass
+
+@dataclass(str)
+class U: pass
 
 @dataclass
-class V:
-    _1 : int
-    _2 : int
+class V: pass
+
+t = T(1,2,3)
+print(t)
+u = U("fun")
+print(u)
 
 ST = Union[S,T]
 UV = Union[U,V]
 
 def claim(f):
     return f
-def match(f):
+def define(f):
     return f
 
 @claim
-def fun(_ : Callable[[ST,UV],float]): pass
+def fun(_ : Lam[[ST,UV],float]): pass
 
 
-@match
+@define
 def fun(_1 : S(x), _2 : V(_,_)):
     return 44
 
-@match
+@define
+def fun(_1 : [S(x), x > 0],  _2 : V(_,_)):
+    return 44
+
+@define
 def fun(_1 : T(_,x,_), _2 : V(y,_)):
     return 45
+
+@claim
+def factorial(_ : Lam[[int],int]): pass
+
+@define
+def factorial(_1 : [n, n == 0]): return 1
+@define
+def factorial(_1 : [n, n > 0]): return n*factorial(n-1)
+
