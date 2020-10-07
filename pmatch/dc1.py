92daacc9f1b6a32656163fa9106be7c683b6fbd9
Evghenii <egx@egx-mbp.local>
2020-10-06 17:36:56 -0700

    checkpoint

--- pmatch/dc1.py
+++ pmatch/dc1.py
@@ -1,19 +1,26 @@
+from typing import Iterator as TIter, Tuple as TTuple, cast as _cast, \
+                   TypeVar as TTypeVar, Union as TUnion
 from dataclasses import dataclass
-from typing import Iterable as TIter, Tuple as TTuple, cast as _cast, \
-                   TypeVar as TTypeVar
 
-_T = TTypeVar("_T")
 
 @dataclass
-class S:
+class S1:
     x : int
     y : str
+    def __iter__(self) -> TIter[TUnion[int, str]]:
+        yield self.x
+        yield self.y
     def unpack(self) -> TTuple[int,str]:
-        return (self.x,self.y)
+        return (self.x, self.y)
 
-s = S(3,"4")
+s = S1(3,"4.0")
 print(s)
+
+def fun(x : int ,y : str) -> None:
+    print("fun", x,y)
 
 a,b = s.unpack()
 print(a,b)
 print(type(a),type(b))
+
+fun(a,b)
