7150a00bda4d98aa6af6b3e20ffad99c9f4937d4
Evghenii <egx@egx-mbp.local>
2020-10-06 22:26:53 -0700

    +1

--- pmatch/tree.py
+++ pmatch/tree.py
@@ -3,14 +3,22 @@ from dataclasses import dataclass
 
 Tree = Union["Leaf", "Node"]
 
+
 @dataclass
-class Leaf: pass
+class Leaf:
+    pass
+
 
 @dataclass
 class Node:
-    value : int
-    left : Tree
-    right : Tree
+    value: int
+    left: Tree
+    right: Tree
 
-tree = Node(32, Node(34, Leaf(), Node(33, Leaf(), Leaf())), Node(44, Node(77,Leaf(), Leaf()), Leaf()))
+
+tree = Node(
+    32,
+    Node(34, Leaf(), Node(33, Leaf(), Leaf())),
+    Node(44, Node(77, Leaf(), Leaf()), Leaf()),
+)
 print(tree)
