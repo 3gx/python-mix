5d61a6e19793e7f9aa13a727aa95b45e36f0d91b
Evghenii <egx@egx-mbp.local>
2020-10-06 22:26:30 -0700

    add tree

---
+++ pmatch/tree.py
@@ -0,0 +1,16 @@
+from typing import List, Union
+from dataclasses import dataclass
+
+Tree = Union["Leaf", "Node"]
+
+@dataclass
+class Leaf: pass
+
+@dataclass
+class Node:
+    value : int
+    left : Tree
+    right : Tree
+
+tree = Node(32, Node(34, Leaf(), Node(33, Leaf(), Leaf())), Node(44, Node(77,Leaf(), Leaf()), Leaf()))
+print(tree)
